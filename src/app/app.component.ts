2edaab990f1b73daa7994e76c043edfecdc1097a
Mahesh EU <mahesheu@gmail.com>
2021-09-20 20:21:54 +0530

    add new data structure to the tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation, ElementRef } from '@angular/core';
 import { DialogComponent, AnimationSettingsModel } from '@syncfusion/ej2-angular-popups';
-import { detach, isNullOrUndefined } from '@syncfusion/ej2-base';
-import { ButtonComponent } from '@syncfusion/ej2-angular-buttons';
+// import { detach, isNullOrUndefined } from '@syncfusion/ej2-base';
+// import { ButtonComponent } from '@syncfusion/ej2-angular-buttons';
 import { SortService, ResizeService, PageService, EditService, ExcelExportService, GridComponent, FreezeService, PdfExportService, ContextMenuService } from '@syncfusion/ej2-angular-grids';
 // import { TreeGrid, Resize, ExcelExport, PdfExport, Edit, Page, ContextMenu, Sort } from '@syncfusion/ej2-treegrid';
 import { MenuEventArgs } from '@syncfusion/ej2-navigations';
@@ -16,6 +16,16 @@ import { sampleData } from './jsontreedata';
 })
 export class AppComponent implements OnInit {
   @ViewChild('grid') public grid: GridComponent;
+  @ViewChild('template') public Dialog: DialogComponent;
+  @ViewChild('field') public field: ElementRef;
+  @ViewChild('headerText') public headerText: ElementRef;
+
+  public showCloseIcon: Boolean = true;
+  public height = '55%';
+  public target = '.control-section';
+  public animationSettings: AnimationSettingsModel = { effect: 'Zoom' };
+  public width = '435px';
+
 
   public data: Object[];
   public contextMenuItems: any;
@@ -28,50 +38,99 @@ export class AppComponent implements OnInit {
   public frozenRow: number = 0;
   public frozenColumn: number = 0;
   public editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal' };
-  // public selectionOptions: SelectionSettingsModel = { type: 'Multiple', mode: 'Cell', cellSelectionMode: 'Flow' };
+  public selectionOptions: { type: 'Multiple', mode: 'Cell', cellSelectionMode: 'Box' };
 
 
   ngOnInit(): void {
     this.data = sampleData;
     this.contextMenuItems = ['Edit', 'Delete', 'Save', 'Copy',
       // { text: 'Copy', target: '.e-content', id: 'copy', iconCss: 'e-icons e-copy' },
-
-      // { text: 'Freeze row', target: '.e-content', id: 'freezerow' },
       { text: 'Freeze On/Off', target: '.e-headercontent', id: 'freezecolumn', iconCss: 'e-icons e-freeze' },
       { text: 'Add Column', target: '.e-headercontent', id: 'addcolumn', iconCss: 'e-icons e-freeze' },
+      { text: 'Del Column', target: '.e-headercontent', id: 'delcolumn', iconCss: 'e-icons e-freeze' },
       { text: 'Filter On/Off', target: '.e-headercontent', id: 'filter', iconCss: 'e-icons e-filter' },
       { text: 'Multi-Sort On/Off', target: '.e-headercontent', id: 'sort', iconCss: 'e-icons e-sort' },
       { text: 'style', target: '.e-headercontent', id: 'style', iconCss: 'e-icons e-style' },
-      { text: 'Paste as sibbling', target: '.e-content', id: 'paste', iconCss: 'e-icons e-copy' }
+      { text: 'Paste as new item', target: '.e-content', id: 'paste', iconCss: 'e-icons e-copy' }
     ];
 
   }
   contextMenuClick(args: MenuEventArgs): void {
 
-    let column = this.grid.getColumnByField(args['column'].field); // Get column object.
+    let column = this.grid.getColumnByField(args['column'].field); // Get
+    if (args.item.id === 'delcolumn') {
+      column.visible = false;
+      this.grid.refreshColumns();
+    }
     if (args.item.id === 'freezecolumn') {
 
-      this.frozenColumn = this.frozenColumn > 0 ? 0 : args['column'].index + 1;
+      //this.getIndex(column.field);
+      this.frozenColumn = this.frozenColumn > 0 ? 0 : this.getIndex(column.field) + 1;
     }
-    if (args.item.id === 'freezerow') {
-      this.frozenRow = args['rowInfo'].cellIndex;
-    }
+
     if (args.item.id === 'filter') {
       this.allowFiltering = !this.allowFiltering;
+    }
+    if (args.item.id === 'style') {
+      this.Dialog.show();
     }
     if (args.item.id === 'sort') {
       this.allowMultiSorting = !this.allowMultiSorting;
     }
     if (args.item.id === 'addcolumn') {
-      let rowDetails: any = { field: 'new', headerText: 'new*', foreignKeyField: 'new' };
-      // rowDetails['field'] = 'new';
-      // rowDetails['headerText'] = 'new';
-      this.grid.columns.push(rowDetails);
-      console.log(this.grid.columns);
-      this.grid.refreshColumns();
-    }
-    if (args.item.id === 'paste') {
+      this.Dialog.show();
 
     }
+    if (args.item.id === 'paste') {
+      let newData = this.getDataFromClipBoard();
+      //console.log(args['rowInfo'].rowData.childRecords.push(newData));
+      this.data.push(newData);
+      setTimeout(() => this.grid.refresh(), 500);
+    }
   }
+  public addColumn = (): void => {
+    let field = this.field.nativeElement.value;
+    let headerText = this.headerText.nativeElement.value
+    let rowDetails: any = { field: field, headerText: headerText, defaultValue: 0 };
+    this.grid.columns.push(rowDetails);
+    this.grid.refreshColumns();
+    this.Dialog.hide();
+
+  }
+  public updateStyle = (): void => {
+    // this.bgColor.nativeElement.value = '';
+    // this.colWidth.nativeElement.value = '';
+  }
+  public getDataFromClipBoard(): object {
+
+    var myobj = {};
+    navigator['clipboard'].readText().then((data) => {
+      //console.log(data);
+      let lines = data.split('\n');
+      console.log(lines);
+      let req = lines.pop().split('\t')
+      var output = this.getColumns().map(function (obj, index) {
+        myobj[obj] = req[index];
+      });
+    });
+
+    return myobj;
+  }
+  public getIndex(colName: string): number {
+    let index = -1;
+    this.grid.columns.forEach(function (arrayItem, key) {
+      if (arrayItem['field'] === colName) {
+        index = key;
+      }
+    });
+    return index;
+  }
+  public getColumns() {
+    let columns = [];
+    this.grid.columns.forEach(function (arrayItem, key) {
+      columns.push(arrayItem['field']);
+    });
+    return columns;
+  }
+
 }
